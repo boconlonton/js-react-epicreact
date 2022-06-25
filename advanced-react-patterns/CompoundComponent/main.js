0ce5f21d552036c2376290026333a9ccbb08fa7a
bocon <boconlonton111@gmail.com>
2022-06-25 13:56:05 +0700

    Update - advanced-patterns - Add ContextModuleFunction, CompoundComponents

---
+++ advanced-react-patterns/CompoundComponent/main.js
@@ -0,0 +1,53 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle(props) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  // 🐨 replace this with a call to React.Children.map and map each child in
+  // props.children to a clone of that child with the props they need using
+  // React.cloneElement.
+  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
+  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
+  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
+  return React.Children.map(props.children, (child, index) => {
+    return React.cloneElement(child, {
+      on: on,
+      toggle: toggle
+    })
+  })
+}
+
+// 🐨 Flesh out each of these components
+
+// Accepts `on` and `children` props and returns `children` if `on` is true
+const ToggleOn = ({on, children}) => on ? children : null
+
+// Accepts `on` and `children` props and returns `children` if `on` is false
+const ToggleOff = ({on, children}) => !on ? children : null
+
+// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
+const ToggleButton = ({on, toggle, ...props}) => <Switch on={on} onClick={toggle} {...props}/>
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <ToggleButton/>
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
